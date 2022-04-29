e71859abbe8c7c8146edf3df3b7af02b9980162f
iQonsult <75740974+iQonsult@users.noreply.github.com>
2022-04-29 08:46:33 +0200

    Added services for/and extended applicationuser, scaffolded MS Identity and added dbcontext. WIP, no extra frontend fields added yet

---
+++ hakimslivs/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using hakimslivs.Data;
+using hakimslivs.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(hakimslivs.Areas.Identity.IdentityHostingStartup))]
+namespace hakimslivs.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ hakimslivs/Models/ApplicationUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace hakimslivs.Models
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+                       ApplyFormatInEditMode = true)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+        public int UsernameChangeLimit { get; set; } = 10;
+    }
+}

050511da5a6412cf97719f20491b11d152376118
mikaelaolsson <mikaelajolsson@hotmail.com>
2022-05-05 18:51:56 +0200

    Added Product Manager and begun CRUD

---
+++ hakimslivs/Pages/Admin/ProductManager/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using hakimslivs.Data;
+using hakimslivs.Models;
+
+namespace hakimslivs.Pages.Admin.ProductManager
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly hakimslivs.Data.ApplicationDbContext _context;
+
+        public DeleteModel(hakimslivs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Item Item { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Item = await _context.Items.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Item == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Item = await _context.Items.FindAsync(id);
+
+            if (Item != null)
+            {
+                _context.Items.Remove(Item);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

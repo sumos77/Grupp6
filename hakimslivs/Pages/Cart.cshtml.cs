a365c0d56ef4da0b75d2a7250b63d6b00ea28043
mikaelaolsson <mikaelajolsson@hotmail.com>
2022-05-10 19:47:36 +0200

    Start of (static) cart page

---
+++ hakimslivs/Pages/Cart.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using hakimslivs.Data;
+using hakimslivs.Models;
+
+namespace hakimslivs.Pages
+{
+    public class CartModel : PageModel
+    {
+        private readonly hakimslivs.Data.ApplicationDbContext _context;
+
+        public CartModel(hakimslivs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Item> Items { get;set; }
+        public IList<Category> Categories { get;set; }
+        public string CurrentCategory { get; set; }
+
+        public async Task OnGetAsync(string currentCategory)
+        {
+            if (!String.IsNullOrEmpty(currentCategory))
+            {
+                Items = await _context.Items.Include(i => i.Category).Where(i => i.Category.Name == currentCategory).ToListAsync();
+            }
+            else
+            {
+                Items = await _context.Items.Include(i => i.Category).ToListAsync();
+            }
+            Categories = await _context.Categories.ToListAsync();
+        }
+    }
+}

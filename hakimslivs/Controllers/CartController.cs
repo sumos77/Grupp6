c28ef0f8fce782b2e4d3b8351c62067e288ac165
Marcus <medina.jobb@outlook.com>
2022-05-12 00:57:29 +0200

    Cart will update when page is loaded Cart Controller will generate a JSON list All items are sorted by name Ugly javascript generates the Cards Plus and minus and trash buttons doesn't work yet

--- hakimslivs/Controllers/CartController.cs
+++ hakimslivs/Controllers/CartController.cs
@@ -1,14 +1,108 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+
+using hakimslivs.Data;
+using hakimslivs.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace hakimslivs.Controllers
 {
-    public class CartController : Controller
+    
+    // Flytta den här klassen till /Models senare
+    internal class CartItems
     {
+        public Item Item { get; set; }
+        public int Amount { get; set; }
+    }
 
-        //[WebMethod]
-        public static string GetCart(string JsonLocalStorageObj)
+    [Route("[controller]")]
+    [ApiController]
+    public class CartreadController : Controller
+    {
+        ApplicationDbContext context;
+        public CartreadController(ApplicationDbContext context)
         {
-            return JsonLocalStorageObj;
+            // Inject database context
+            this.context = context;
+        }
+
+        [HttpPut]
+        [Route("/GetCartItems/{json?}")]
+        public object GetCartItems([FromBody] object jObject)
+        {
+            // Lista som ska returneras
+            var items = new List<CartItems>();
+            if(jObject != null)
+            {
+                items = GetListWithItems(jObject);
+            }
+            
+            // Returnera listan till javascriptet i JSON form
+            return JsonConvert.SerializeObject(items);
+        }
+
+        //TODO: Anropa denna från Javascript när betalningen är klar
+        [HttpPut]
+        [Route("/GenerateOrder/{json?}")]
+        public object GenerateOrder([FromBody] object jObject)
+        {
+            // Exempeldata
+            // {"shopping-cart":"{"1":1,"2":1}","i18nextLng":"en-US"}
+
+            var success = false;
+            var items = new List<CartItems>();
+
+            // Lista som ska returneras
+            if(jObject != null)
+            {
+                items = GetListWithItems(jObject);
+                //TODO: Spara beställning i databasen
+                
+                // Om allt gått bra sätt sucess till true och javascriptet
+                // får rensa kundkorgen!
+                success = true;
+            }
+
+            return JsonConvert.SerializeObject(success);
+        }
+
+        private List<CartItems> GetListWithItems(object jObject)
+        {
+            List<CartItems> items=new();
+            // Konvertera objektet till string och sedan till en Dictionary
+            var jsonString = jObject.ToString();
+            var data = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+            foreach(var item in data)
+            {
+                if(item.Key == "shopping-cart")
+                {
+                    var cart = item.Value;
+                    // Konvertera shoppingkarten till en Dictionary
+                    var cartList = JsonConvert.DeserializeObject<Dictionary<string, int>>(cart);
+                    foreach(var cartItem in cartList)
+                    {
+                        // Omvandla ID till int då den skickas in som sträng
+                        _ = int.TryParse(cartItem.Key, out int id);
+                        // Hitta varan som kunden vill ha
+                        var curItem = context.Items.FirstOrDefault(item => item.ID == id);
+                        if(curItem != null)
+                        {
+                            // Lägg till varan i listan om den inte är null
+                            items.Add(new CartItems { Item = curItem, Amount = cartItem.Value });
+                        }
+                    }
+                }
+            }
+            
+            // Sortera varorna i listan efter namn
+            items = items.OrderBy(item => item.Item.Product).ToList();
+            return items;
         }
 
         //public IActionResult Index()

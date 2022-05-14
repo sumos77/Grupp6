209d8269d5a3dbe8f130d3df2bfa5e7ba7bbc088
Martin <48153829+sumos77@users.noreply.github.com>
2022-05-14 17:30:31 +0200

    Added javascript for search´functionality

--- hakimslivs/wwwroot/js/site.js
+++ hakimslivs/wwwroot/js/site.js
@@ -4,6 +4,40 @@ let shoppingCartElt = document.getElementsByClassName('bi-cart')[0];
 readLocalStorage();
 allAddButtons = document.getElementsByClassName('add-to-cart');
 
+//Search by category and product whenever a button is pressed and redraws the page.
+function searchOnClick(){
+    let searchIndex = [];
+    //Builds two arrays from what is currently displayed on the page
+    currentPageProducts = Array.from(document.getElementsByClassName('product-search-name'));
+    currentPageCategories = Array.from(document.getElementsByClassName('category-link'));
+    //Turns all the values in those two arrays into lowercase and add them to the searchIndex list
+    currentPageProducts.forEach(item => searchIndex.push(item.textContent.toLowerCase()));
+    currentPageCategories.forEach(item => searchIndex.push(item.textContent.toLowerCase()));
+    //The result is an array-like object created by the funtion getElementsByClassName, there's only one search bar so we only need the first one
+    inputElt = document.getElementsByClassName('search-bar-content')[0];
+    
+    //Whenever a key is released this eventhandler fires
+    inputElt.addEventListener('keyup', (event) =>{
+        searchTerm = inputElt.value.toLowerCase();
+        const result = searchIndex.filter(word => word.includes(searchTerm));
+        //All cards that are currently displayed
+        currentPageCards = Array.from(document.getElementsByClassName('card'));
+        //Filters which cards to display
+        currentPageCards.forEach(item => {
+            cardTitle = item.getElementsByClassName('product-search-name')[0].textContent.toLowerCase();
+            cardCategory = item.getElementsByClassName('category-link')[0].textContent.toLowerCase();
+            if (result.includes(cardTitle) || result.includes(cardCategory)){
+                item.style.display='flex';
+            }
+            else {
+                item.style.display = 'none';
+            }
+            
+        })
+    });
+}
+
+
 function registerHandlers() {
     for (const addButton of allAddButtons) {
         addButton.onclick = event => {
@@ -61,3 +95,4 @@ function numberOfItemsInCart() {
 
 registerHandlers();
 numberOfItemsInCart();
+searchOnClick();
